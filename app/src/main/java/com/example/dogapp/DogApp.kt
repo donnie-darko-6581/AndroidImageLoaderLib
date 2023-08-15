767e551cdf3a315ce51e6db199475cf6629cb566
lalit <patil.lalit.v3@gmail.com>
2023-08-15 22:01:52 +0530

    set application class and init lib
    - set app class to init lib

---
+++ app/src/main/java/com/example/dogapp/DogApp.kt
@@ -0,0 +1,13 @@
+package com.example.dogapp
+
+import android.app.Application
+import com.example.dogimageloader.DogImageLib
+
+class DogApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DogImageLib.getInstance()
+    }
+}
